a4495e6c4ca700eafff13e4979dde1f223d1870f
Paulo Sergio de Jesus Tolentino <paulo.tolentino@venturus.org.br>
2022-02-17 18:38:26 -0300

    refact footer to allow scroll

--- code/react/src/pages/Home/index.tsx
+++ code/react/src/pages/Home/index.tsx
@@ -2,15 +2,16 @@ import React from "react";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import { CommentsList } from "../../components/CommentsList/index";
+import "./index.css";
 
 type HomeType = {};
 
 const Home: React.FC<HomeType> = () => (
-  <>
+  <div className="home">
     <Header />
     <CommentsList />
     <Footer />
-  </>
+  </div>
 );
 
 export default Home;
