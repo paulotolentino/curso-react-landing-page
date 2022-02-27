fc5508d56de008e97f31fcb1a5f0d1850a500e09
Murilo Ferreira Frizzarin <murilo.frizzarin@gmail.com>
2022-02-27 12:21:01 -0300

    Lazy load

--- code/react/src/App.tsx
+++ code/react/src/App.tsx
@@ -1,24 +1,43 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import "./App.css";
-import Home from "./pages/Home";
-import Program from "./pages/Program";
-import Students from "./pages/Students";
-import Team from "./pages/Team";
 
 const App: React.FC = () => {
-  // TODO Lazy?
-  // const lazyHome = React.lazy(() => import("./pages/Home"));
+  const { t } = useTranslation();
+
+  const LazyHome = React.lazy(() => import("./pages/Home"));
+  const LazyProgram = React.lazy(() => import("./pages/Program"));
+  const LazyStudents = React.lazy(() => import("./pages/Students"));
+  const LazyTeam = React.lazy(() => import("./pages/Team"));
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<Navigate to="/" />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/students" element={<Students />} />
-        <Route path="/program" element={<Program />} />
-        <Route path="/team" element={<Team />} />
+        <Route path="*" element={<Navigate to="/home" />} />
+        {/* Router V6:
+         https://reactrouter.com/docs/en/v6/examples/lazy-loading#preview */}
+        <Route path="/home" element={
+            <React.Suspense fallback={<>{t('loading')}</>}>
+                <LazyHome />
+              </React.Suspense>
+          } />
+        <Route path="/students" element={
+            <React.Suspense fallback={<>{t('loading')}</>}>
+                <LazyStudents />
+              </React.Suspense>
+          } />
+        <Route path="/program" element={
+            <React.Suspense fallback={<>{t('loading')}</>}>
+                <LazyProgram />
+              </React.Suspense>
+          } />
+        <Route path="/team" element={
+            <React.Suspense fallback={<>{t('loading')}</>}>
+                <LazyTeam />
+              </React.Suspense>
+          } />
       </Routes>
     </BrowserRouter>
   );
